4865671af979e8f140b1456c97bb7fa72b81e41d
Chrissy Wainwright <cwainwright@gmail.com>
2011-11-18 14:10:19 -0500

    fixed name of templates dir, updated a couple default values

--- sixieskel/theme/template.py
+++ sixieskel/theme/template.py
@@ -5,7 +5,7 @@ from zopeskel.plone3_theme import Plone3Theme
 
 
 class SixieTheme(Plone3Theme):
-    _template_dir = 'templatesj'
+    _template_dir = 'templates'
     summary = "A Custom Six Feet Up Theme"
 
     vars = copy.deepcopy(Plone3Theme.vars)
@@ -21,15 +21,16 @@ class SixieTheme(Plone3Theme):
     get_var(vars, 'package').description = 'The package contained namespace package (like policy or theme)'
     get_var(vars, 'package').default = 'theme'
     get_var(vars, 'zope2product').default = False
-    get_var(vars, 'description').default = 'An installable theme for Plone 3'
+    get_var(vars, 'description').default = 'An installable theme for Plone 4'
     get_var(vars, 'author').default = 'Six Feet Up, Inc.'
     get_var(vars, 'author_email').default = 'info@sixfeetup.com'
     get_var(vars, 'url').default = 'http://www.sixfeetup.com'
     get_var(vars, 'keywords').default = 'web zope plone theme'
-    get_var(vars, 'version').default = '0.1dev'
+    get_var(vars, 'version').default = '0.1'
     get_var(vars, 'skinname').description = 'The skin selection to be added to portal_skins (like SFUP Theme)'
     get_var(vars, 'skinbase').default = 'Plone Basic'
     get_var(vars, 'empty_styles').default = False
+    get_var(vars, 'include_doc').default = False
 
     def post(self, command, output_dir, vars):
         if '.svn' in os.listdir(output_dir):
