3a28f6acfd1b2393a229fbb4694a1c42d7f83401
Clayton Parker <clayton@sixfeetup.com>
2012-03-01 15:32:53 -0500

    PEP8

--- sixieskel/theme/template.py
+++ sixieskel/theme/template.py
@@ -1,4 +1,6 @@
-import os, copy, subprocess
+import os
+import copy
+import subprocess
 from zopeskel.vars import StringVar
 from zopeskel.base import get_var
 from zopeskel.plone3_theme import Plone3Theme
@@ -15,10 +17,13 @@ class SixieTheme(Plone3Theme):
             default='4.1',
            )
     )
-    get_var(vars, 'namespace_package').description = 'Namespace package (like plonetheme or Products)'
-    get_var(vars, 'namespace_package').description = 'Namespace package (like plonetheme or Products)'
+    get_var(vars, 'namespace_package').description = (
+        'Namespace package (like plonetheme or Products)')
+    get_var(vars, 'namespace_package').description = (
+        'Namespace package (like plonetheme or Products)')
     get_var(vars, 'namespace_package').default = 'Products'
-    get_var(vars, 'package').description = 'The package contained namespace package (like policy or theme)'
+    get_var(vars, 'package').description = (
+        'The package contained namespace package (like policy or theme)')
     get_var(vars, 'package').default = 'theme'
     get_var(vars, 'zope2product').default = False
     get_var(vars, 'description').default = 'An installable theme for Plone 4'
@@ -27,7 +32,8 @@ class SixieTheme(Plone3Theme):
     get_var(vars, 'url').default = 'http://www.sixfeetup.com'
     get_var(vars, 'keywords').default = 'web zope plone theme'
     get_var(vars, 'version').default = '0.1'
-    get_var(vars, 'skinname').description = 'The skin selection to be added to portal_skins (like SFUP Theme)'
+    get_var(vars, 'skinname').description = (
+        'The skin selection to be added to portal_skins (like SFUP Theme)')
     get_var(vars, 'skinbase').default = 'Plone Basic'
     get_var(vars, 'empty_styles').default = False
     get_var(vars, 'include_doc').default = False
